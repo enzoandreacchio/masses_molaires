3b73d0c46b180f6f9d6f7d02c3b05250b2d536a9
zozzo8 <enzochio8@gmail.com>
2019-04-14 14:28:04 +0200

    testughjkg

--- Cryptographie integrale/decodage_crypto_integrale.py
+++ Cryptographie integrale/decodage_crypto_integrale.py
@@ -9,10 +9,28 @@ def f(x):
 #///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+# Définition de la fonction qui permet de calculer l'intégrale par parties d'une fonction f sur un intervalle [a;b]
+def integ(f, a, b):
+    output = 0
+    inf = 0
+    sup = 0
+    sec = (b - a) / 100000
+    i = a
+    while i <= b:
+        inf += f(i) * sec
+        i += sec
+        sup += f(i) * sec
+    return (inf + sup) / 2
+
+
+# Input du message à décoder
 msg_code = input("Entrez le message à décoder : ")
+
+# Création d'une liste contenant tous les coefficients du polynôme transmis
 liste_coeffs = msg_code.split("e")
 
 
+# Création de la fonction polynômiale transmise
 def g(x):
     r = 0
     for i in range(0, len(liste_coeffs)):
@@ -20,9 +38,13 @@ def g(x):
     return r
 
 
+# Création de la fonction qui sera intégrée pour donner l'aire entre les deux courbes
 def h(x):
     return (math.copysign(f(x) - g(x), 1))
 
-
+# Output des valeurs obtenues
 for i in range(1, len(liste_coeffs) + 1):
+    # Valeur calculé grâce à la méthode d'intégration codée ci-dessus
+    print(integ(h, i - 1, i))
+    # Valeur calculée grâce à la fonction de spicy.integrate
     print(quad(h, i - 1, i)[0])
